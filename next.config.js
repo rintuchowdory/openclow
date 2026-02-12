9c0beb4c203d659332e9c82b6ca58da11ad94c53
rintuchowdory <chowdoryrintu60@gmail.com>
2026-02-12 01:21:58 +0100

    Add static export config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
   images: {
-    unoptimized: true,
-  },
-  trailingSlash: true,
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
